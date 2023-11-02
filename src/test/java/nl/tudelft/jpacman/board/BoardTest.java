a40415525dadb54a0b54b29909553dd0a597ea94
Waris Damkham <99647936+Waariss@users.noreply.github.com>
2023-11-02 14:18:55 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -3,7 +3,9 @@ package nl.tudelft.jpacman.board;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-
+/***
+*
+*/
 public class BoardTest {
 
     private Board board;
