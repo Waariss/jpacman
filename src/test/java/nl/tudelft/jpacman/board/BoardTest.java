a513bdaaf6527f8a57e0a2c832b36d5582e4568a
Waris Damkham <99647936+Waariss@users.noreply.github.com>
2023-11-02 14:16:42 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -7,30 +7,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BoardTest {
 
     private Board board;
-
+    /***
+    *
+    */
     @BeforeEach
     void setUp() {
         Square[][] grid = new Square[1][1];
         grid[0][0] = new BasicSquare();
         board = new Board(grid);
     }
-
+    /***
+    *
+    */
     @Test
     void verifyBoardSize() {
         assertThat(board.getWidth()).isEqualTo(1);
         assertThat(board.getHeight()).isEqualTo(1);
     }
-
+    /***
+    *
+    */
     @Test
     void squareAtReturnsCorrectSquare() {
         assertThat(board.squareAt(0, 0)).isInstanceOf(BasicSquare.class);
     }
-
+    /***
+    *
+    */
     @Test
     void invariantHolds() {
         assertThat(board.invariant()).isTrue();
     }
-
+    /***
+    *
+    */
     @Test
     void squareAtReturnsNullSquare() {
         Square s1 = new BasicSquare();
